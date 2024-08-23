aed617e2dfe2ec970631be16fe811a6fd4b0b7ed
Vladimirkasperovich <vova.kasper.boy@gmail.com>
2024-08-23 18:06:37 +0300

    feat: add store for scroll, and add react use library

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -1,23 +1,34 @@
+'use client'
 import React from 'react';
 import {cn} from "@/lib/utils";
 import Link from "next/link";
+import {useCategoryStore} from "@/store/category";
 
 
 interface Props {
     className?: string;
 }
 
-const cats = ['Пиццы', 'Комбо', 'Закуски', 'Коктейли', 'Кофе', 'Напитки', 'Десерты', 'Десерты'];
-const activeIndex = 0;
+const cats = [
+    { id: 1, name: 'Пиццы' },
+    { id: 2, name: 'Комбо' },
+    { id: 3, name: 'Закуски' },
+    { id: 4, name: 'Коктейли' },
+    { id: 5, name: 'Кофе' },
+    { id: 6, name: 'Напитки' },
+    { id: 7, name: 'Десерты' },
+    { id: 8, name: 'Десерты' }
+];
 
 export const Categories: React.FC<Props> = ({className}) => {
+    const {activeId} = useCategoryStore()
     return (
         <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
             {
-                cats.map((cat, index) => (
-                    <Link key={index} href={'#'}
-                          className={cn('flex items-center font-bold h-11 rounded-2xl px-5', activeIndex === index && 'bg-white shadow-md shadow-gray-200 text-primary')}>
-                        <button>{cat}</button>
+                cats.map(cat => (
+                    <Link key={cat.id} href={'#'}
+                          className={cn('flex items-center font-bold h-11 rounded-2xl px-5', activeId === cat.id && 'bg-white shadow-md shadow-gray-200 text-primary')}>
+                        <button>{cat.name}</button>
                     </Link>
                 ))
             }
--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,7 +1,9 @@
+'use client'
 import React from 'react';
 import {Title} from "@/components/shared/title";
-import {cn} from "@/lib/utils";
+import {useIntersection} from 'react-use'
 import {ProductCard} from "@/components/shared/product-card";
+import {useCategoryStore} from "@/store/category";
 
 interface Props {
     className?: string;
@@ -17,8 +19,21 @@ export const ProductsGroupList: React.FC<Props> = ({
                                                        title,
                                                        items
                                                    }) => {
+    const setActiveCategoryId = useCategoryStore(state => state.setActiveId)
+    const intersectionRef = React.useRef(null);
+    const intersection = useIntersection(intersectionRef, {
+        threshold: 0.4,
+    })
+
+    React.useEffect(() => {
+        if (intersection?.isIntersecting) {
+            setActiveCategoryId(categoryId)
+            console.log(title, categoryId)
+        }
+    }, [title, categoryId, intersection?.isIntersecting])
+
     return (
-        <div className={className}>
+        <div className={className} id={title} ref={intersectionRef}>
             <Title text={title} size="lg" className="font-extrabold mb-5"/>
             <div className="grid grid-cols-3 gap-[50px]">
                 {items.map((product, i) => (
