695f0456173860f225a414d974051c688b58d150
Vladimirkasperovich <vova.kasper.boy@gmail.com>
2024-09-16 18:37:33 +0300

    feat: make extra layouts for route groups

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+
+import {Nunito} from "next/font/google";
+import "./globals.css";
+
+const nunito = Nunito({
+    subsets: ["cyrillic"],
+    variable: '--font-nunito',
+    weight: ['400', '500', '600', '700', '800', '900'],
+}
+);
+
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+        <head>
+            <link rel="icon" href="/logo.png" data-rh='true'/>
+        </head>
+        <body className={nunito.className}>{children}</body>
+        </html>
+    );
+}
